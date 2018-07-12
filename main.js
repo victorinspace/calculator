9a1876d27624ec057da5809c0b303ae554239c8e
Victor Velasquez <victorv24@gmail.com>
2018-07-12 08:24:07 -0700

    mas

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $(function() {
 
 	// Adding
-	$('#add').on('click', function() {
-		var num1 = parseInt($('#num_one').val(), 10)
-		var num2 = parseInt($('#num_two').val(), 10)
-		var add = num1 + num2
+	// $('#add').on('click', function() {
+	// 	var num1 = parseInt($('#num_one').val(), 10)
+	// 	var num2 = parseInt($('#num_two').val(), 10)
+	// 	var add = num1 + num2
 
-		$('.calculator__display').text(add)
-	})
+	// 	$('.calculator__display').text(add)
+	// })
 
-	$('#four').on('click', function() {
+
 		
-		$('#num_input').val('#four')	
-	})
+	$('#num_input').val('4 + 4')	
+
 	
 
 })
